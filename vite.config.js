9f0a1f50aee19fba2452845d73851ccfe1ca51a5
Mozeu <menyraconn@gmail.com>
2026-05-02 20:25:09 -0600

    Fix 1

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // Change 'reading-tracker' to your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/reading-tracker/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
+  base: process.env.VITE_BASE ?? '/',
 })
